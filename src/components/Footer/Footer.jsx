3936eecb12be226d495e586e39ec5a11f9c665a5
FGeaorg <g29f05@gmail.com>
2022-11-30 22:40:47 +0800

    layout AboutMe, Portfolio, Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,22 @@
 import './Footer.css';
 
 function Footer() {
-
+  return (
+    <footer className="footer">
+      <h5 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h5>
+      <div className="footer__wrapper">
+        <p className="footer__copyright">© 2022. FGeorg</p>
+        <ul className="footer__link-list">
+          <li className="footer__item">
+            <a href="https://practicum.yandex.ru/" className="footer__link">Яндекс.Практикум</a>
+          </li>
+          <li className="footer__item">
+            <a href="https://github.com/FGeorgy" className="footer__link">Github</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,7 +1,22 @@
 import './Portfolio.css';
 
 function Portfolio() {
-
+  return (
+    <section className="portfolio">
+      <h4 className="portfolio__title">Портфолио</h4>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a href="1" className="portfolio__link">Статичный сайт</a>
+        </li>
+        <li className="portfolio__item">
+          <a href="2" className="portfolio__link">Адаптивный сайт</a>
+        </li>
+        <li className="portfolio__item">
+          <a href="3" className="portfolio__link">Одностраничное приложение</a>
+        </li>
+      </ul>
+    </section>
+  )
 };
 
 export default Portfolio;
